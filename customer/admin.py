62db9db94042babfda08fe98dae725322297c047
rd-king <dharmikradadiya05@gmail.com>
2025-04-02 23:43:30 +0530

    test

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.fields]  # Automatically show all columns
--- measurement/admin.py
+++ measurement/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Measurement, Order
 
-# Register your models here.
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.fields]  # Automatically show all columns
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.fields]  # Automatically show all columns
